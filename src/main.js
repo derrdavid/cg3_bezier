5f3bd6b746a8a8b4b50da19e1cfdfb204e5f6e76
David Derr <david.derr@stud.hs-hannover.de>
2024-06-20 11:31:22 +0200

    add SphereManager

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { BezierCurve } from './utils/bezier';
 import ToggleCamera from './systems/camera';
 import { setupRenderer } from './systems/renderer';
-import { setupControls } from './systems/controls';
+import { setupControls, SphereManager } from './systems/controls';
 
 const scene = new THREE.Scene();
 const canvas = document.getElementById('canvas-container');
@@ -18,23 +18,13 @@ const toggleButton = document.getElementById('switchButton');
 const animateButton = document.getElementById('animate-button');
 
 let isAnimate = false;
-let control_points = [];
 
-const updateControlPoints = () => {
-    inputFields.forEach((field, index) => {
-        const values = field.value.split(',').map(Number);
-        if (values.length === 3) {
-            const point = new THREE.Vector3(values[0], values[1], values[2]);
-            control_points[index] = point;
-        } else {
-            console.error('Each input field must have exactly three values separated by commas.');
-        }
-    });
-};
+const sphereManager = new SphereManager(scene, camera, inputFields);
 
 updateButton.addEventListener('click', () => {
-    updateControlPoints();
-    bezierCurve.controlPoints = control_points;
+    sphereManager.updateControlPoints();
+    console.log(sphereManager.controlPoints);
+    //bezierCurve.controlPoints = control_points;
 });
 
 toggleButton.addEventListener('click', () => {
@@ -48,65 +38,17 @@ animateButton.addEventListener('click', () => {
     isAnimate = !isAnimate;
 });
 
-// Sphere
-const points = [new THREE.Vector3(1, 1, 1), new THREE.Vector3(2, 2, 1)];
-const spheres = [];
-points.forEach((point) => {
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-    const sphere = new THREE.Mesh(geometry, material);
-    sphere.position.set(point.x, point.y, point.z);
-    scene.add(sphere);
-    spheres.push(sphere);
+inputFields.forEach((field, index) => {
+    field.addEventListener('change', () => {
+        sphereManager.updateControlPoints();
+    });
 });
 
-// Raycaster
-const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
-
-// Mouse Inputs
-function onPointerMove(event) {
-    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
-}
-
-let isDragging = false;
-
-window.addEventListener('pointermove', onPointerMove);
-
-window.addEventListener('mousedown', (event) => {
-    raycaster.setFromCamera(pointer, camera.current);
-    const intersects = raycaster.intersectObjects(spheres);
-    if (intersects.length > 0) {
-        isDragging = true;
-    }
-});
-
-window.addEventListener('mouseup', () => {
-    isDragging = false;
-});
-
-window.addEventListener('mousemove', (event) => {
-    if (isDragging && camera.current == camera.ortho) {
-        raycaster.setFromCamera(pointer, camera.current);
-        const intersects = raycaster.intersectObjects(spheres);
-        if (intersects.length > 0) {
-            const intersect = intersects[0];
-            console.log(intersect)
-            // Calculate the new position in screen space
-            const newPoint = new THREE.Vector3(pointer.x, pointer.y, intersect.point.z);
-            newPoint.unproject(camera.current);
-            intersect.object.position.x = newPoint.x;
-            intersect.object.position.y = newPoint.y;
-        }
-    }
-});
-
+const bezierCurve = new BezierCurve(sphereManager.controlPoints, scene);
 const axesHelper = new THREE.AxesHelper(20);
 scene.add(axesHelper);
 
 let t = slider.value;
-let sphereLabel = document.getElementById('sphere-label');
 
 function animate() {
     if (isAnimate) {
@@ -116,6 +58,8 @@ function animate() {
     }
     controls.update();
     renderer.render(scene, camera.current);
+    console.log(slider.value)
+    bezierCurve.update(slider.value);
     requestAnimationFrame(animate);
 }
 
--- src/systems/controls.js
+++ src/systems/controls.js
@@ -1,4 +1,5 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
 
 export function setupControls(camera, domElement) {
     const controls = new OrbitControls(camera, domElement);
@@ -7,4 +8,99 @@ export function setupControls(camera, domElement) {
     controls.screenSpacePanning = false;
     controls.maxPolarAngle = Math.PI / 2;
     return controls;
+}
+export class SphereManager {
+    constructor(scene, camera, inputFields) {
+        this.scene = scene;
+        this.camera = camera;
+        this.inputFields = inputFields;
+        this.controlPoints = [];
+        this.spheres = [];
+        this.boundingSpheres = [];
+        this.isDragging = false;
+        this.draggedSphereIndex = -1;
+
+        this.updateControlPoints();
+        this.createSpheres();
+
+        window.addEventListener('pointermove', this.onPointerMove.bind(this));
+        window.addEventListener('mousedown', this.onMouseDown.bind(this));
+        window.addEventListener('mouseup', this.onMouseUp.bind(this));
+        window.addEventListener('mousemove', this.onMouseMove.bind(this));
+
+        this.raycaster = new THREE.Raycaster();
+        this.pointer = new THREE.Vector2();
+    }
+
+    updateControlPoints() {
+        this.inputFields.forEach((field, index) => {
+            const values = field.value.split(',').map(Number);
+            if (values.length === 3) {
+                const point = new THREE.Vector3(values[0], values[1], values[2]);
+                this.controlPoints[index] = point;
+                if (this.spheres[index]) {
+                    this.spheres[index].position.set(point.x, point.y, point.z);
+                    this.boundingSpheres[index].center.set(point.x, point.y, point.z);
+                }
+            } else {
+                console.error('Each input field must have exactly three values separated by commas.');
+            }
+        });
+    }
+
+    createSpheres() {
+        this.controlPoints.forEach((point, index) => {
+            const geometry = new THREE.SphereGeometry(0.5, 32, 32);  // Sphere with smaller radius
+            const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+            const sphere = new THREE.Mesh(geometry, material);
+            sphere.position.set(point.x, point.y, point.z);
+            this.scene.add(sphere);
+            this.spheres.push(sphere);
+
+            // Create a bounding sphere for raycasting
+            const boundingSphere = new THREE.Sphere(point, 0.1);
+            this.boundingSpheres.push(boundingSphere);
+        });
+    }
+
+    onPointerMove(event) {
+        this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+        this.pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    }
+
+    onMouseDown(event) {
+        this.raycaster.setFromCamera(this.pointer, this.camera.current);
+        const intersects = this.raycaster.intersectObjects(this.spheres);
+        if (intersects.length > 0 && this.camera.current == this.camera.ortho) {
+            this.isDragging = true;
+            this.draggedSphereIndex = this.spheres.indexOf(intersects[0].object);
+        }
+    }
+
+    onMouseUp() {
+        this.isDragging = false;
+        this.draggedSphereIndex = -1;
+    }
+
+    onMouseMove() {
+        if (this.isDragging && this.draggedSphereIndex !== -1) {
+            this.raycaster.setFromCamera(this.pointer, this.camera.current);
+            const intersects = this.raycaster.intersectObjects(this.spheres);
+            if (intersects.length > 0) {
+                const sphere = this.spheres[this.draggedSphereIndex];
+                const newPoint = new THREE.Vector3(this.pointer.x, this.pointer.y, sphere.position.z);
+                newPoint.unproject(this.camera.current);
+                if (this.camera.current.isOrthographicCamera) {
+                    newPoint.z = sphere.position.z;
+                }
+                sphere.position.set(newPoint.x, newPoint.y, newPoint.z);
+                const { x, y, z } = sphere.position;
+                this.controlPoints[this.draggedSphereIndex].set(x, y, z);
+                this.inputFields[this.draggedSphereIndex].value = `${x.toFixed(1)},${y.toFixed(1)},${z.toFixed(1)}`;
+
+                // Update bounding sphere position
+                this.boundingSpheres[this.draggedSphereIndex].center.set(x, y, z);
+            }
+        }
+    }
 }
